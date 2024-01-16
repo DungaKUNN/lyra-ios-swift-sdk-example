19a469dc3f2cab178995ad464c2d3feff4c34ff6
Jeanpier <66151424+DungaKUNN@users.noreply.github.com>
2024-01-16 11:11:44 -0500

    Update2 LyraKeys.swift

--- demoPayment/Helpers/LyraKeys.swift
+++ demoPayment/Helpers/LyraKeys.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 struct LyraKeys {
-    static let publicKey    = ""  //Contraseña Publica de Test
+    static let publicKey    = ""  //Contraseña Publica de Test o Producción
     static let serverName   = "https://api.micuentaweb.pe"  //Nombre del Servidor API REST
     static let urlServer    = ""         //Servidor 
     static let username     = ""  //Usuario
-    static let password     = ""  //Contraseña de Test
+    static let password     = ""  //Contraseña de Test o Producción
     static let cureency     = "PEN"
     static let loginBase64  = String(format: "%@:%@", username, password).data(using: String.Encoding.utf8)?.base64EncodedString()
 }
